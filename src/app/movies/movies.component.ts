db56afd1e7b4edc60506c365c1f46bdd79f35ba2
Colin Cook and Luke Vandekieft <colin@>
2018-10-30 16:22:15 -0700

    Setup dynamic router to deliver data to components.

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { WikiService } from '../wiki.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-movies',
@@ -12,11 +16,20 @@ export class MoviesComponent implements OnInit {
 
   movies: FirebaseListObservable<any[]>;
 
-  constructor(private database: WikiService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private database: WikiService) { }
+  movieKey: string;
+  movieToShow;
 
   ngOnInit() {
     this.movies = this.database.getMovies();
     this.movies.subscribe(res => console.log(res));
-  }
 
+    this.route.params.forEach((urlParameters) => {
+      this.movieKey = urlParameters['$key'];
+    });
+    this.database.getMovieByKey(this.movieKey).subscribe(dataIn => {
+     this.movieToShow = dataIn;
+     console.log(this.movieToShow);
+   })
+ }
 }
