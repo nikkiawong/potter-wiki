4043aaa0a8f1ccdbd634c4afa218c99d05b67c9e
Student <example@example.com>
2018-10-31 16:26:18 -0700

    add button to show add component

--- src/app/add-character/add-character.component.ts
+++ src/app/add-character/add-character.component.ts
@@ -11,8 +11,8 @@ import { Character } from '../models/character.model';
 export class AddCharacterComponent implements OnInit {
 // @Output() sendCharacter = new EventEmitter();
 
-submitForm(characterName: string, characterBirthdate: string, characterFirstAppearance: string, characterLastAppearance: string, characterPortrayedBy: string, characterHouse: string, characterFamily: string[], characterLoveInterest: string, characterMovie: number, characterSummary: string, characterSpoilerSummary: string ) {
-  let newCharacter: Character = new Character(characterName, characterBirthdate, characterFirstAppearance, characterLastAppearance, characterPortrayedBy, characterHouse, characterFamily, characterLoveInterest, characterMovie, characterSummary, characterSpoilerSummary);
+submitForm(characterName: string, characterBirthdate: string, characterFirstAppearance: string, characterLastAppearance: string, characterPortrayedBy: string[], characterHouse: string, characterFamily: string[], characterLoveInterest: string, characterMovie: number, characterMovieSpecificSummary: string[], characterMovieSpecificSpoilerSummary: string[], characterPhoto: string[] ) {
+  let newCharacter: Character = new Character(characterName, characterBirthdate, characterFirstAppearance, characterLastAppearance, characterPortrayedBy, characterHouse, characterFamily, characterLoveInterest, characterMovie, characterMovieSpecificSummary, characterMovieSpecificSpoilerSummary, characterPhoto);
 
   this.wikiService.addCharacter(newCharacter);
 }
